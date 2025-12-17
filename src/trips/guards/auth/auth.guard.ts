956333369e741567a8bd998948e040972940f2e5
Sternenwarte88 <s.loecher.slc@gmail.com>
2025-12-17 14:34:19 +0100

    removed app service, added guard, added httpyak requests

---
+++ src/trips/guards/auth/auth.guard.ts
@@ -0,0 +1,18 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Request } from 'express';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const request: Request = context.switchToHttp().getRequest();
+    const apiKey = request.header('x-api-key');
+
+    if (apiKey !== 'secret123') {
+      return false;
+    }
+    return true;
+  }
+}
--- src/trips/trips.controller.ts
+++ src/trips/trips.controller.ts
@@ -1,5 +1,6 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { CreateTripDTO } from './create-trip.dto';
+import { AuthGuard } from './guards/auth/auth.guard';
 import { TripsService } from './provider/trips.service';
 import { type Trip } from './trips.interface';
 
@@ -12,6 +13,7 @@ export class TripsController {
     return this.tripsService.findAll();
   }
 
+  @UseGuards(AuthGuard)
   @Post('/trips')
   createTrip(@Body() createTripDto: CreateTripDTO): Trip {
     return this.tripsService.create(createTripDto);
